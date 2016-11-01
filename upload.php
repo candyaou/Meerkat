409ae56ca7cbaa5fa5ef82325fa5383aebd1c823
Paspana Assarasee <candychupa_aou@hotmail.com>
2016-11-02 00:02:33 +0700

    First Commit For Meerkat Demo

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php 
+	$con = mysqli_connect("localhost","root","root","meerkat");
+	if($_POST['image']){
+		$data = $_POST['image'];
+		$Fname = $_POST['Fname'];
+		$Lname = $_POST['Lname'];
+		list($type, $data) = explode(';', $data);
+		list(, $data)      = explode(',', $data);
+		$data = base64_decode($data);
+		$filename = $Fname."_".$Lname.".jpg";
+		file_put_contents("img/meerkat/".$filename, $data);
+		$filepath = "img/meerkat/".$filename;
+		$sql = "INSERT INTO meerkat (img_name, img_path, Fname, Lname) VALUES ('$filename','$filepath','$Fname','$Lname')";
+		$result = mysqli_query($con,$sql);
+	}else{
+		$message = "wrong answer";
+		echo "<script type='text/javascript'>alert('$message');</script>";
+	}
+?>
